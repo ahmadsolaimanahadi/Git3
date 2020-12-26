2f962146a8a4b31dd5bbf2f70f6ceec1cc368545
ahmad <ahmad@10.0.0.151>
2020-12-25 23:13:49 -0500

    Way Formula(Physics)

---
+++ src/Part2/Part2.java
@@ -0,0 +1,15 @@
+package Part2;
+
+public class Part2 {
+	
+	static int speed = 100;
+	static int time = 10;
+	int way;
+	
+	public static void main(String[] args) {
+		
+		int way = speed/time;
+		System.out.println("way is equal to speed/time: " + way);
+	}
+
+}
